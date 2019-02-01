01e535e39b517a1cf3d5141c2a823f15cb4b8685
shamsrk <mdshamsreza69@gmail.com>
2019-02-01 11:02:30 +0530

    phpunit master: Remove Response from controllers

--- src/AppBundle/Controller/users/LoginController.php
+++ src/AppBundle/Controller/users/LoginController.php
@@ -10,7 +10,6 @@ use AppBundle\Services\Validator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Constants\KeyConstants as Key;
 
